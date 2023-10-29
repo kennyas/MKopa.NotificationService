10c4d95cee00a577f5358ef8ea42fba249f05d08
Asishanak <kehinde.asishana@accessbankplc.com>
2023-10-30 00:23:43 +0100

    included test project

---
+++ SmsServiceTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SmsServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
